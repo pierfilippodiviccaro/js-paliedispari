25f3a0e2b00c53ef3647c4d38734a7e97672eed2
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-03 15:58:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// esercizio numero 1: Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma (NON usare split, reverse e join)
+/*passaggi: 1) chiedo all'utente il nome, 2) creo una funzione per capire se è palindroma la parola. 3) se è palindroma allora va avanti, sennò di accesso negato
+*/
+/*function isWordPalindrome(word) {
+    let lunghezza = word.length;
+    for ( let i = 0; i < lunghezza / 2; i++)
+        if (word[i] !== word[lunghezza - i - 1]) {
+            return false;
+        }
+        else {
+            return true;
+        }
+}
+
+let wordUser = prompt("inserisci una parola palindroma")
+if (isWordPalindrome(wordUser)) {
+    console.log("aaccesso consentito")
+}
+ else{
+    console.log("accesso negato")
+}
+*/
+//  esercizio numero 2: Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
